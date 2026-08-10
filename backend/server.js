ec7f6d621ad0bf588dda7ff314015ef123b621c8
Aditya Patil <aditya29890patil@gmail.com>
2026-08-10 10:31:02 +0530

    Connect backend with database on mongodb atlas.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
 const app = express();
 app.use(express.json());
+
+connectDB();
 
 const PORT = 5000;
 
